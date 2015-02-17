6ddb1f39ed681d536359f3891c9ff456a0348573
smehmoodf3 <smehmood@folio3.com>
2015-02-17 19:58:38 +0500

    commit

--- suitelet/suitelet_devA.js
+++ suitelet/suitelet_devA.js
@@ -23,13 +23,8 @@ nlapiLogExecution('debug','Log added by developer 2');
 function getRecords()
 {
 
-<<<<<<< HEAD
-=======
-nlapiLogExecution('debug','Added log Execution line');
 var rec=nlapiSearchRecord('salesorder');
->>>>>>> 7224bba08fb84b0e6c14181aa449e4722ee02b85
 
-var rec=nlapiSearchRecord('salesorder');
 
 
 }
